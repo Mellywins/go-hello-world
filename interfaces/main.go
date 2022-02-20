9f5c3e5263f04c68a8add8a70fa606628361331d
Mellywins <zouaghi.wow1919@gmail.com>
2022-02-20 15:59:04 +0100

    interfaces

---
+++ interfaces/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Shape interface {
+	Area() float64
+}
+type Circle struct {
+	x, y, radius float64
+}
+type Rectangle struct {
+	width, height float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+func getArea(s Shape) float64 {
+	return s.Area()
+}
+
+func main() {
+	circle := Circle{x: 0, y: 0, radius: 5}
+	rectangle := Rectangle{width: 10, height: 5}
+	fmt.Printf("Circle Area: %f\n", getArea(circle))
+	fmt.Printf("Rectangle Area: %f\n", getArea(rectangle))
+}
